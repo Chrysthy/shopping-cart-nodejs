c746cfcc10c6995cc5da38febdb45b5249199b8f
Chrystine Martins <chrystine.martins@outlook.com>
2025-06-13 20:44:30 -0300

    import all cart.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import createItem  from "./services/item.js";
+import createItem from "./services/item.js";
+import * as cartServices from "./services/cart.js";
 
-const cart = [];
+const myCart = [];
 
 console.log("Welcome to the shopping cart system!");
 
 const item1 = await createItem("Silent Hill 2 - Game", 299.99, 1);
 const item2 = await createItem("Controle PS5", 399.99, 2);
+
+cartServices.addItem(myCart)
 
 console.log(item2.subtotal());
 
