8d5f351527ecc10b4ef6f34fcbfe4334675af423
Chrystine Martins <chrystine.martins@outlook.com>
2025-06-13 20:33:54 -0300

    create 2 items to test createItem function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import createItem  from "./services/item.js";
+
 const cart = [];
+
+console.log("Welcome to the shopping cart system!");
+
+const item1 = await createItem("Silent Hill 2 - Game", 299.99, 1);
+const item2 = await createItem("Controle PS5", 399.99, 2);
+
+console.log(item2.subtotal());
+
