7236490e611dcb4092d8a1bb18c047519a7184f4
Chrystine Martins <chrystine.martins@outlook.com>
2025-06-13 20:32:48 -0300

    create objeto, uma subtotal function dentro do return e export function

--- src/services/item.js
+++ src/services/item.js
@@ -3,4 +3,12 @@
 // -> criar item com subtotal calculado
 async function createItem (name, price, quantity) {
 
+    return {
+        name,
+        price,
+        quantity,
+        subtotal: () => price * quantity,
+    }
 }
+
+export default createItem;
