45943f7f9f2bdfa57d7fced9a6079b322ea163c7
Chrystine Martins <chrystine.martins@outlook.com>
2025-06-15 09:13:10 -0300

    update code index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import createItem from "./services/item.js";
-import * as cartServices from "./services/cart.js";
+import * as cartService from "./services/cart.js";
 
 const myCart = [];
+//const myWhishlist = [];
 
-console.log("Welcome to the shopping cart system!");
+console.log("\nWelcome to the shopping cart system!\n");
 
 const item1 = await createItem("Silent Hill 2 - Game", 299.99, 1);
-const item2 = await createItem("Controle PS5", 399.99, 2);
+const item2 = await createItem("Controle PS5", 399.99, 3);
+const item3 = await createItem("Cheetos 1kg", 25.99, 2);
 
-cartServices.addItem(myCart)
 
-console.log(item2.subtotal());
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myCart, item2);
+await cartService.addItem(myCart, item3);
+
+
+await cartService.displayCart(myCart);
+
+
+await cartService.deleteItem(myCart, item3.name);
+await cartService.displayCart(myCart);
+
+
+await cartService.removeItem(myCart, item2);
+await cartService.removeItem(myCart, item3);
+
+
+await cartService.displayCart(myCart);
+
+
+await cartService.calculateTotal(myCart);
 
